4c2334845f1ab0895b28877c1d6ce703732954ca
Jonathan Grimes <grimesj16@students.ecu.edu>
2019-01-26 16:27:26 -0500

    Base classes created for queen and rook

---
+++ Queen.java
@@ -0,0 +1,14 @@
+/* Queen movement pattern:
+ *      X  X  X
+ *       X X X
+ *        XXX
+ *XXXXXXXXXOXXXXXXXXX
+ *        XXX
+ *       X X X
+ *      X  X  X
+ */
+
+public class Queen extends Piece
+{
+    //TODO: add queen specific functions
+}
---
+++ Rook.java
@@ -0,0 +1,14 @@
+/* Rook movement pattern:
+ *         X
+ *         X
+ *         X
+ *XXXXXXXXXOXXXXXXXXX
+ *         X
+ *         X
+ *         X
+ */
+
+public class Rook extends Piece
+{
+    //TODO: add rook specific functions
+}
