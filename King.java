d62061411001f29c28b9b75082695029cc4fd0eb
Patrick Philips <jppwriter01@gmail.com>
2019-01-26 13:12:04 -0500

    Created King class

---
+++ King.java
@@ -0,0 +1,11 @@
+/* 
+ * The King is the most important piece on the chess board.
+ * This piece moves one square in any direction.
+ * If it is placed in check by an opponent's piece, the King is threatened with capture.
+ * The player must then move the King out of check.
+ * Failure to do so leads to CheckMate and end of game.
+ */
+
+public class King extends Piece {
+    // TODO: Define special behavior for King
+}
