20498e6762a32b263b1b4ce600dcc14a8663acf5
Patrick Philips <jppwriter01@gmail.com>
2019-01-26 22:29:10 -0500

    Added move(), capture(), and captured() methods to Bishop.java

--- Bishop.java
+++ Bishop.java
@@ -3,4 +3,17 @@
 public class Bishop extends Piece {
 
     // TODO: Add Bishop specific functionality
+    public void move(String location) {
+        // Changes the piece's location to a new square
+        this.location = location;
+    }
+
+    public void capture() {
+        // if a piece exists on the square to which this piece
+        // is moved, that piece is captured.
+    }
+
+    public void captured() {
+        // Not sure if this is required; if it is not, it will be removed
+    }
 }
