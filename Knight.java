46af8972717ef372911fd0b5bb295bed6668540c
Joshua Reyes <reyesjos16@students.ecu.edu>
2019-01-31 16:38:15 -0500

    Creation of basic pawn and knight class

---
+++ Knight.java
@@ -0,0 +1,13 @@
+public class Knight extends Piece{
+        public Knight(){
+            super();
+    }
+    public void move(){
+    }
+    public void capture(){
+
+    }
+    public void captured(){
+
+    }
+}
