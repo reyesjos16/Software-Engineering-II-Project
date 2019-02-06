d8910edbf7b4a98b458ec86095e1c7b3832eccca
Brandon Lee Underwood <underwoodb.sd@gmail.com>
2019-02-06 10:39:54 -0500

    Rebuilt Piece with coordinates, board, player, and image path.

--- Piece.java
+++ Piece.java
@@ -1,5 +1,5 @@
 /*
-*   General Piece is an abstract class that provides each game piece with
+*   Piece is an abstract class that provides each game piece with
 *   common functions.
 *
 *
@@ -7,40 +7,66 @@
 
 public abstract class Piece
 {
-    public Piece(int xCor, int yCor) {
-        this.xcoordinate = xCor;
-        this.ycoordinate = yCor;
+
+    // Member variables
+    protected int xcoordinate;  // X coordinate
+    protected int ycoordinate;  // Y coordinate
+    protected String name;      // Piece name (knight, rook, pawn, king, queen, bishop)
+    protected Player player;    // Player this piece belongs to (black, white?)
+    protected String imagePath; // Path to piece image, for GUI purposes
+    protected Board board;      // Current chess board
+
+    // Constructor
+    public Piece(String pieceName, Player player, Board board) {
+        this.name = pieceName;
+        this.player = player;
+        this.name = name;
+        this.xcoordinate = -1;   // Set location to "null"
+        this.ycoordinate = -1;   // TODO: Discuss what should be used for null value?
+        this.board = board;
     }
-    
-    // Current location of piece on the board
-    protected int xcoordinate;
-    protected int ycoordinate;
 
     // Abstract functions each piece must have
     
-    // TODO: Define what input for move is required
-    public abstract void move(int newX, int newY);
+    // Changed move() to validMoveList(). Might make more sense for the player to move,
+    // and each piece knows it's own valid moves
+    public abstract ArrayList<Tile> validMoveList();
 
-    // TODO: Define input for capture and captured
-    // TODO: Is captured required?
+    // TODO: Decide if capture() and captured() are still necessary
+
+    // Capture opponents piece
     public abstract void capture();
 
+    // Recognize and handle being captured
     public abstract boolean captured();
 
-    // Location getter
-    public int[] getLoc() {
-        arrLocation = new int[2];
-        arrLocation[0] = this.xcoordinate;
-        arrLocation[1] = this.ycoordinate;
-        return arrLocation;
-    }
+    /* Getters */
 
-    // Location setter
-    public void setLoc(int newX, int newY) {
-        this.xcoordinate = newX;
-        this.ycoordinate = newY;
-    }
+    public int getXcoordinate() { return this.xcoordinate; }
 
+    public int getYCoordinate() { return this.ycoordinate; }
+
+    public String getName() { return this.name; }
+
+    public Player getPlayer() { return this.player; }
+
+    public String getImagePath() { return this.imagePath; }
+
+    public String getBoard() { return this.board; }
+
+    /* Setters */
+
+    public void setXCoordinate(int newX) { this.xcoordinate = newX; }
+
+    public void setYCoordinate(int newY) { this.ycoordinate = newY; }
+
+    public void setName(String pieceName) { this.name = pieceName; }
+
+    public void setPlayer(Player player) { this.player = player; }
+
+    public void setImagePath(String imagePath) { this.imagePath = imagePath; }
+
+    public void setBoard(Board board) { this.board = board; }
     // King will implement it's own "threatened" method
 
     // Special moves (since not all pieces have them) should be handled for each piece
