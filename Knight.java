181636ad4a2744de5c0def3dd2a5c41a199f9317
Joshua Reyes <reyesjos16@students.ecu.edu>
2019-02-26 17:40:30 -0500

    Added movement for Pawn and Knight

--- Knight.java
+++ Knight.java
@@ -1,6 +1,7 @@
-public class Knight extends Piece{
+import java.util.ArrayList;
+public class Knight extends Piece {
 
-    Knight(int x, int y, Player p, Board b){
+    Knight(int x, int y, Player p, Board b) {
         super("knight", p, b);
         this.setXcoordinate(x);
         this.setYcoordinate(y);
@@ -8,11 +9,37 @@ public class Knight extends Piece{
 
     private boolean captured = false;
 
-    public void move(int newX, int newY){
-
-
+    public void capture() {
+        captured = true;
     }
-    public void capture(){captured = true;}
-    public boolean captured(){return captured;}
 
+    public boolean captured() {
+        return captured;
+    }
+
+    private void movement(int x, int y, ArrayList<Tile> moveList){
+        Tile nextMove = board.getTile(this.getXcoordinate()+x,this.getYcoordinate()+y);
+        if(nextMove != null){
+            if(!nextMove.isEmpty()){
+                if(!nextMove.getPiece().getPlayer().getColor().equals(this.getPlayer().getColor())){
+                    moveList.add(nextMove);
+                }
+            }
+            else
+            moveList.add(nextMove);
+        }
+    }
+
+    public ArrayList<Tile> validMoveList() {
+        ArrayList<Tile> moveList = new ArrayList<>();
+            movement(1,2,moveList);
+            movement(2,1 ,moveList);
+            movement(2,-1,moveList);
+            movement(1,-2,moveList);
+            movement(-1,-2,moveList);
+            movement(-2,-1, moveList);
+            movement(-2,1, moveList);
+            movement(-1, 2, moveList);
+        return moveList;
+    }
 }
