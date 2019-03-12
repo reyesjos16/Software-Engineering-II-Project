ebb8dcb3acea010bae7232c50b8bd59afff3988b
Jonathan Grimes <grimesj16@students.ecu.edu>
2019-03-12 17:28:48 -0400

    Fixed indentation and removed exception

--- ChessGUI.java
+++ ChessGUI.java
@@ -1,6 +1,3 @@
-
-package mychess;
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
@@ -11,6 +8,7 @@ import java.awt.Image;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.AbstractAction;
@@ -32,40 +30,42 @@ public class ChessGUI {
 
     private final JPanel gui = new JPanel(new BorderLayout(3, 3));
     private JButton[][] chessBoardSquares = new JButton[8][8];
-    
+
     private Image[][] chessPieceImages = new Image[2][6];
     private JPanel chessBoard;
-    
+
+    public static final String IMG_PATH = "./pieces/";
+
     public static final String wrk="white_rook.png", brk="black_rook.png", wbs="white_bishop.png",bbs="black_bishop.png",
-                    whr="white_knight.png", bhr="black_knight.png",wp="white_pawn.png",bp="black_pawn.png",wk="white_king.png",
-                    bk="black_king.png", wq="white_queen.png",bq="black_queen.png"; 
+            whr="white_knight.png", bhr="black_knight.png",wp="white_pawn.png",bp="black_pawn.png",wk="white_king.png",
+            bk="black_king.png", wq="white_queen.png",bq="black_queen.png";
     private static final String COLS = "ABCDEFGH";
     public static final int QUEEN = 0, KING = 1,
             ROOK = 2, KNIGHT = 3, BISHOP = 4, PAWN = 5;
     public static final int[] STARTING_ROW = {
-        ROOK, KNIGHT, BISHOP, KING, QUEEN, BISHOP, KNIGHT, ROOK
+            ROOK, KNIGHT, BISHOP, KING, QUEEN, BISHOP, KNIGHT, ROOK
     };
     public static final int BLACK = 0, WHITE = 1;
 
-    
-    ChessGUI() 
+
+    ChessGUI()
     {
         initializeGui();
     }
 
-    public final void initializeGui() 
+    public final void initializeGui()
     {
-        
+
         // set up the main GUI
         gui.setBorder(new EmptyBorder(5, 5, 5, 5));
         JToolBar tools = new JToolBar();
         tools.setFloatable(false);
         gui.add(tools, BorderLayout.PAGE_START);
-        Action newGameAction = new AbstractAction("New") 
+        Action newGameAction = new AbstractAction("New")
         {
 
             @Override
-            public void actionPerformed(ActionEvent e) 
+            public void actionPerformed(ActionEvent e)
             {
                 setupNewGame();
             }
@@ -79,11 +79,11 @@ public class ChessGUI {
 
         gui.add(new JLabel("?"), BorderLayout.LINE_START);
 
-        chessBoard = new JPanel(new GridLayout(0, 9)) 
+        chessBoard = new JPanel(new GridLayout(0, 9))
         {
 
             @Override
-            public final Dimension getPreferredSize() 
+            public final Dimension getPreferredSize()
             {
                 Dimension d = super.getPreferredSize();
                 Dimension prefSize = null;
@@ -93,11 +93,11 @@ public class ChessGUI {
                             (int)d.getWidth(),(int)d.getHeight());
                 } else if (c!=null &&
                         c.getWidth()>d.getWidth() &&
-                        c.getHeight()>d.getHeight()) 
+                        c.getHeight()>d.getHeight())
                 {
                     prefSize = c.getSize();
-                } 
-                else 
+                }
+                else
                 {
                     prefSize = d;
                 }
@@ -111,7 +111,7 @@ public class ChessGUI {
         chessBoard.setBorder(new CompoundBorder(
                 new EmptyBorder(8,8,8,8),
                 new LineBorder(Color.BLACK)
-                ));
+        ));
         // Set the BG to be ochre
         Color ochre = new Color(204,119,34);
         chessBoard.setBackground(ochre);
@@ -122,21 +122,21 @@ public class ChessGUI {
 
         // create the chess board squares
         Insets buttonMargin = new Insets(0, 0, 0, 0);
-        for (int ii = 0; ii < chessBoardSquares.length; ii++) 
+        for (int ii = 0; ii < chessBoardSquares.length; ii++)
         {
-            for (int jj = 0; jj < chessBoardSquares[ii].length; jj++) 
+            for (int jj = 0; jj < chessBoardSquares[ii].length; jj++)
             {
                 JButton b = new JButton();
                 b.setMargin(buttonMargin);
-               
+
                 ImageIcon icon = new ImageIcon(
                         new BufferedImage(50, 50, BufferedImage.TYPE_INT_ARGB));
-                
+
                 b.setIcon(icon);
                 b.setPreferredSize(new Dimension(60,60));
                 if ((jj % 2 == 1 && ii % 2 == 1)
-                      
-                        || (jj % 2 == 0 && ii % 2 == 0)) 
+
+                        || (jj % 2 == 0 && ii % 2 == 0))
                 {
                     b.setBackground(Color.PINK);
                 } else {
@@ -151,16 +151,16 @@ public class ChessGUI {
          */
         chessBoard.add(new JLabel(""));
         // fill the top row
-        for (int ii = 0; ii < 8; ii++) 
+        for (int ii = 0; ii < 8; ii++)
         {
             chessBoard.add(
                     new JLabel(COLS.substring(ii, ii + 1),
-                    SwingConstants.CENTER));
+                            SwingConstants.CENTER));
         }
         // fill the black non-pawn piece row
-        for (int ii = 0; ii < 8; ii++) 
+        for (int ii = 0; ii < 8; ii++)
         {
-            for (int jj = 0; jj < 8; jj++) 
+            for (int jj = 0; jj < 8; jj++)
             {
                 switch (jj) {
                     case 0:
@@ -172,89 +172,91 @@ public class ChessGUI {
             }
         }
     }
-     private final void setupNewGame() 
-     {
-            String P;
-        for(int i=0;i<8;i++)
-			for(int j=0;j<8;j++)
-			{	
-                            P = null;
-				if(i==0&&j==0)
-					P=brk;
-				else if(i==0&&j==7)
-					P=brk;
-				else if(i==7&&j==0)
-					P=wrk;
-				else if(i==7&&j==7)
-					P=wrk;
-				else if(i==0&&j==2)
-					P=bbs;
-				else if (i==0&&j==5)
-					P=bbs;
-				else if(i==7&&j==2)
-					P=wbs;
-				else if (i==7&&j==5)
-					P=wbs;
-				else if(i==0&&j==1)
-					P=bhr;
-				else if (i==0&&j==6)
-					P=bhr;
-				else if(i==7&&j==1)
-					P=whr;
-				else if(i==7&&j==6)
-					P=whr;
-				else if(i==0&&j==3)
-					P=bk;
-				else if(i==0&&j==4)
-					P=bq;
-				else if(i==7&&j==3)
-					P=wk;
-				else if(i==7&&j==4)
-					P=wq;
-				else if(i==1)
-                                        P=bp;
-				else if(i==6)
-					P=wp;
-                                
-                                     try {
-    Image img = ImageIO.read(getClass().getResource(P));
-     Image newimg = img.getScaledInstance( 40, 40,  java.awt.Image.SCALE_SMOOTH ) ;  
-
-    chessBoardSquares[j][i].setIcon(new ImageIcon(newimg));
-  } 
-                                     catch (Exception ex) 
-  {
-    System.out.println(ex);
-  }
+    private final void setupNewGame()
+    {
+        String P;
+        for(int y=0;y<8;y++)
+        {
+            P=null;
+            for(int x=0;x<8;x++)
+            {
+                if(y==0&&x==0)
+                    P=brk;
+                else if(y==0&&x==7)
+                    P=brk;
+                else if(y==7&&x==0)
+                    P=wrk;
+                else if(y==7&&x==7)
+                    P=wrk;
+                else if(y==0&&x==2)
+                    P=bbs;
+                else if (y==0&&x==5)
+                    P=bbs;
+                else if(y==7&&x==2)
+                    P=wbs;
+                else if (y==7&&x==5)
+                    P=wbs;
+                else if(y==0&&x==1)
+                    P=bhr;
+                else if (y==0&&x==6)
+                    P=bhr;
+                else if(y==7&&x==1)
+                    P=whr;
+                else if(y==7&&x==6)
+                    P=whr;
+                else if(y==0&&x==3)
+                    P=bk;
+                else if(y==0&&x==4)
+                    P=bq;
+                else if(y==7&&x==3)
+                    P=wk;
+                else if(y==7&&x==4)
+                    P=wq;
+                else if(y==1)
+                    P=bp;
+                else if(y==6)
+                    P=wp;
+                else
+                    break;
+                try {
+                    Image img = ImageIO.read(new File(IMG_PATH+P));
+                    Image newimg = img.getScaledInstance( 40, 40,  java.awt.Image.SCALE_SMOOTH ) ;
+                    chessBoardSquares[x][y].setIcon(new ImageIcon(newimg));
+                }
+                catch (Exception ex)
+                {
+                    System.out.println(ex);
+                }
+            }
         }
-        
-			}
-    
-		
 
-    public final JComponent getGui() 
+    }
+
+
+
+    public final JComponent getGui()
     {
         return gui;
     }
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
-        Runnable r = new Runnable() 
+        Runnable r = new Runnable()
         {
 
             @Override
-            public void run() 
+            public void run()
             {
                 ChessGUI cg = new ChessGUI();
 
                 JFrame f = new JFrame("ChessChamp");
 
                 f.add(cg.getGui());
-                
+
                 f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
                 f.setLocationByPlatform(true);
 
                 f.pack();
-                
+
                 f.setMinimumSize(f.getSize());
                 f.setVisible(true);
             }
