7561a731c3e15767c2923cb2601399c40fbda679
Patrick Philips <jppwriter01@gmail.com>
2019-03-19 12:59:40 -0400

    Added MVC mappings and Web Security classes

---
+++ eevilchess/src/main/java/projecteevee/eevilchess/MvcConfig.java
@@ -0,0 +1,17 @@
+package projecteevee.eevilchess;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("index");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/profile").setViewName("profile");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
