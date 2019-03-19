b17bbbd36623147366e90b7cab7d1668e0d5723a
Patrick Philips <jppwriter01@gmail.com>
2019-03-19 15:05:18 -0400

    Corrected URL mappings for homepage

--- eevilchess/src/main/java/projecteevee/eevilchess/MvcConfig.java
+++ eevilchess/src/main/java/projecteevee/eevilchess/MvcConfig.java
@@ -8,8 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("index");
-        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
         registry.addViewController("/profile").setViewName("profile");
         registry.addViewController("/login").setViewName("login");
     }
