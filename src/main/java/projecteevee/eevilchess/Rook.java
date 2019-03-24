5f96dc8e50c0634c333aa3612f0074af0978b623
Patrick Philips <jppwriter01@gmail.com>
2019-03-24 01:17:37 -0400

    Created copy of src in repository root for Travis-CI build debugging

---
+++ src/main/java/projecteevee/eevilchess/Rook.java
@@ -0,0 +1,53 @@
+/* Rook movement pattern:
+ *         X
+ *         X
+ *         X
+ *XXXXXXXXXOXXXXXXXXX
+ *         X
+ *         X
+ *         X
+ */
+package projecteevee.eevilchess;
+
+import java.util.ArrayList;
+
+public class Rook extends Piece
+{
+    Rook(int x, int y, Player p, Board b)
+    {
+        super("rook", p, b);
+        this.setXcoordinate(x);
+        this.setYcoordinate(y);
+    }
+
+    private boolean captured = false;
+
+    public ArrayList<Tile> validMoveList()
+    {
+        ArrayList<Tile> movelist = new ArrayList<>();
+        //up
+        movelist.addAll(getValidMoves(this, 0, 1));
+        //right
+        movelist.addAll(getValidMoves(this, 1, 0));
+        //down
+        movelist.addAll(getValidMoves(this, 0, -1));
+        //left
+        movelist.addAll(getValidMoves(this, -1, 0));
+        return movelist;
+    }
+
+    public void move(int x, int y)
+    {
+
+    }
+
+    public void capture()
+    {
+        captured = true;
+    }
+
+    public boolean captured()
+    {
+        return captured;
+    }
+}
---
+++ src/main/java/projecteevee/eevilchess/Tile.java
@@ -0,0 +1,39 @@
+package projecteevee.eevilchess;
+
+public class Tile
+{
+    int x,y;
+    private Piece holding;
+
+    Tile(int xcord, int ycord)
+    {
+        x = xcord;
+        y = ycord;
+        holding = null;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public boolean isEmpty()
+    {
+        return holding == null;
+    }
+
+    public Piece getPiece()
+    {
+        return holding;
+    }
+
+    public void setPiece(Piece newpiece)
+    {
+        holding = newpiece;
+    }
+}
