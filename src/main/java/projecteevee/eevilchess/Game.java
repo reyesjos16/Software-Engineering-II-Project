db0fb797a0d0d533eceba10e10450c280767c58b
Patrick Philips <jppwriter01@gmail.com>
2019-04-01 23:32:30 -0400

    Added Game class for GameController

---
+++ src/main/java/projecteevee/eevilchess/Game.java
@@ -0,0 +1,13 @@
+package projecteevee.eevilchess;
+
+public class Game
+{
+    private String game_id;
+    private String player1_id;
+    private String player2_id;
+    private String time_per_turn;
+    private String board_dimensions;
+    private Boolean randomize_pieces;
+
+}
+
