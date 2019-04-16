54bd2053ddf6332a1f6ddc3cde4d0df10aa14f93
Patrick Philips <jppwriter01@gmail.com>
2019-04-15 21:11:54 -0400

    Restored ChessGame files after making them their own package & fixing import issue with Board.java

---
+++ src/main/java/projecteevee/eevilchess/chessgame/King.java
@@ -0,0 +1,78 @@
+/* 
+ * The King is the most important piece on the chess board.
+ * This piece moves one square in any direction.
+ * If it is placed in check by an opponent's piece, the King is threatened with capture.
+ * The player must then move the King out of check.
+ * Failure to do so leads to CheckMate and end of game.
+ */
+package projecteevee.eevilchess.chessgame;
+
+import java.util.ArrayList;
+
+public class King extends Piece {
+    // TODO: Define special behavior for King
+    King(int x, int y, Player p, Board b) {
+        super("king", p, b);
+        this.setXcoordinate(x);
+        this.setYcoordinate(y);
+    }
+    
+    public void capture() {}
+    
+    public boolean captured() {return false;}
+    
+    public Boolean isThreatened() {
+        /* isThreatened() returns true if the King is in check,
+         * and false otherwise.
+         */
+        
+         //TODO: How is this to be implemented? Should we use an Observer pattern?
+         return false;
+    }
+
+    private boolean isCheck(Tile t)
+    {
+        //TODO: returns true if given tile will put king in check
+        return false;
+    }
+
+    // ( 0, 1)
+    // ( 1, 1)
+    // ( 1, 0)
+    // ( 1,-1)
+    // ( 0,-1)
+    // (-1,-1)
+    // (-1, 0)
+    // (-1, 1)
+    private void checkMove(int x, int y, ArrayList<Tile> movelist)
+    {
+        Tile nexttile = this.getBoard().getTile(this.getXcoordinate() + x, this.getYcoordinate() + y);
+        if(nexttile != null)
+        {
+            //Tile exists
+            if(nexttile.isEmpty() || !nexttile.getPiece().getPlayer().getColor().equals(this.getPlayer().getColor()))
+            {
+                //Tile is empty or has an enemy piece on it
+                if(!this.isCheck(nexttile))
+                {
+                    //Tile will not put the king in check
+                    movelist.add(nexttile);
+                }
+            }
+        }
+    }
+
+    public ArrayList<Tile> validMoveList()
+    {
+        ArrayList<Tile> movelist = new ArrayList<>();
+        this.checkMove(0, 1, movelist);
+        this.checkMove(1, 1, movelist);
+        this.checkMove(1, 0, movelist);
+        this.checkMove(1, -1, movelist);
+        this.checkMove(0, -1, movelist);
+        this.checkMove(-1, -1, movelist);
+        this.checkMove(-1, 0, movelist);
+        this.checkMove(-1, 1, movelist);
+        return movelist;
+    }
+}
---
+++ src/main/java/projecteevee/eevilchess/chessgame/Main.java
@@ -0,0 +1,36 @@
+package projecteevee.eevilchess.chessgame;
+
+/*
+ *	Main handles running the Eevil Chess
+ *	Authors: Team Eevee
+ *
+ * */
+
+import java.util.*;
+import projecteevee.eevilchess.chessgame.Board;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // To be replaced by checkmate or threatened function later
+        boolean checkmate = false;
+        Scanner keyboard = new Scanner(System.in);
+
+        // Game is being played
+        while(!checkmate) {
+            Board newboard = new Board(8, 8);
+            newboard.populateTableNormal(new Player("white"), new Player("black"));
+            System.out.println(newboard.getBoardJSON().toString(4));
+            // Temporary check for testing. Enter 1 to break the loop.
+            System.out.println("Continue?\n");
+            int check = keyboard.nextInt();
+            if(check == 1) {
+                checkmate = true;
+            }
+
+            // Player turn code goes here
+
+        }
+    }
+}
