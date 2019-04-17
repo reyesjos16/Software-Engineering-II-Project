7251e4820c63d1110b1af2bfc0abbe09a66dfd47
Patrick Philips <jppwriter01@gmail.com>
2019-04-16 22:25:25 -0400

    Changed GameRepository to extend JPA instead of CRUD repository class

--- src/main/java/projecteevee/eevilchess/GameRepository.java
+++ src/main/java/projecteevee/eevilchess/GameRepository.java
@@ -1,10 +1,13 @@
 package projecteevee.eevilchess;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.repository.CrudRepository;
 
 import projecteevee.eevilchess.Game;
 
-public interface GameRepository extends CrudRepository<Game, Integer>
+//public interface GameRepository extends CrudRepository<Game, Integer>
+public interface GameRepository extends JpaRepository<Game, Integer>
+
 {
     
 }
