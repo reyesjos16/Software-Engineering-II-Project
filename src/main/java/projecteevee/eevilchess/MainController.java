7c87745d56cff53fb34b5ce6bbf84051874c162d
Patrick Philips <jppwriter01@gmail.com>
2019-04-17 23:07:41 -0400

    Changed createNewGame method in MainController to POST mapping

--- src/main/java/projecteevee/eevilchess/MainController.java
+++ src/main/java/projecteevee/eevilchess/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -25,8 +26,8 @@ public class MainController
         String gameID = id;
         return gameRepository.getOne(gameID);
     }
-    
-    @GetMapping(path="/newgame")
+
+    @PostMapping(path="/newgame")
     public @ResponseBody String createNewGame (@RequestParam String gameID, @RequestParam String player1, @RequestParam String player2)
     {
         Game new_game = new Game();
