a4c62e719ad617cacd6db1ecd7be89dc763f492f
Patrick Philips <jppwriter01@gmail.com>
2019-04-18 08:06:09 -0400

    Added curl script for adding games to database

---
+++ new-game.sh
@@ -0,0 +1,3 @@
+curl -d "gameID=1&player1=brandon&player2=josh" -X POST 'localhost:8080/eevee/newgame'
+curl -d "gameID=2&player1=brandon&player2=josh" -X POST 'localhost:8080/eevee/newgame'
+curl -d "gameID=3&player1=james&player2=jonathan" -X POST 'localhost:8080/eevee/newgame'
