d6784f43b4a52b131763d3b74d78ea1c7f2fdd6f
Patrick Philips <jppwriter01@gmail.com>
2019-04-19 14:03:05 -0400

    Reverted to CRUD repository while debugging API endpoints

--- src/main/java/projecteevee/eevilchess/GameRepository.java
+++ src/main/java/projecteevee/eevilchess/GameRepository.java
@@ -1,18 +1,14 @@
 package projecteevee.eevilchess;
 
 import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-//import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.CrudRepository;
 //import org.springframework.data.repository.PagingAndSortingRepository;
 
 import projecteevee.eevilchess.Game;
 
-//public interface GameRepository extends CrudRepository<Game, Integer>
-public interface GameRepository extends JpaRepository<Game, Integer>
+public interface GameRepository extends CrudRepository<Game, Integer>
 {
-    List<Game> findByGameID(@Param("gameID") String gameID);
+    //List<Game> findByGameID(@Param("gameID") String gameID);
     //Game findByGameID(String gameID);
     //@Query(value="SELECT FROM GAME WHERE GAMEID=?0", nativeQuery = true)
     //Game findByGameID(@Param("gid") String gid);
